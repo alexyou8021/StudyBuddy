16cc01aa3c0732ef9fded2a28832d14cea34ab09
Alexander You <alex@samuel-mbp.local>
2017-11-20 23:33:49 -0600

    starting Dms

---
+++ StudyBuddy/DMViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DMViewController.swift
+//  StudyBuddy
+//
+//  Created by Alexander You on 11/20/17.
+//  Copyright © 2017 Alexander You. All rights reserved.
+//
+
+import UIKit
+
+class DMViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
